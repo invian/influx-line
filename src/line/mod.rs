290a5c69911ecfde71d6891e2ff58ea1a678c5d5
DOCtorActoAntohich <fairy666death@mail.ru>
2024-06-17 14:28:26 +0300

    use key name

--- src/line/mod.rs
+++ src/line/mod.rs
@@ -1,8 +1,8 @@
-use std::{collections::HashMap, marker::PhantomData};
+use std::collections::HashMap;
 
 use chrono::{DateTime, Utc};
 
-use crate::{InfluxValue, MeasurementName, NameRestrictionError};
+use crate::{InfluxValue, KeyName, MeasurementName, NameRestrictionError};
 
 /// Implements InfluxDB Line Protocol V2.
 ///
@@ -10,51 +10,71 @@ use crate::{InfluxValue, MeasurementName, NameRestrictionError};
 #[derive(Debug, Clone)]
 pub struct InfluxLine {
     /// Required measurement name.
-    pub measurement: MeasurementName,
+    measurement: MeasurementName,
     /// The original name `Tag Set` is not adapted for simplicity.
-    pub tags: HashMap<String, String>,
+    tags: HashMap<KeyName, KeyName>,
     /// The original name `Field Set` is not adapted for simplicity.
-    pub fields: HashMap<String, InfluxValue>,
+    fields: HashMap<KeyName, InfluxValue>,
     /// [`DateTime`] sounds more readable for a timestamp.
-    pub timestamp: Option<DateTime<Utc>>,
-    /// Fancy constructors are preferable for safery.
-    _phantom: PhantomData<()>,
+    timestamp: Option<DateTime<Utc>>,
 }
 
 impl InfluxLine {
-    pub fn new<K, V>(measurement: MeasurementName, field: K, value: V) -> Self
+    pub fn new<V>(measurement: MeasurementName, field: KeyName, value: V) -> Self
     where
-        K: Into<String>,
         V: Into<InfluxValue>,
     {
-        let fields = [(field.into(), value.into())].into_iter().collect();
+        let fields = [(field, value.into())].into_iter().collect();
         Self {
             measurement,
             tags: HashMap::new(),
             fields,
             timestamp: None,
-            _phantom: PhantomData,
         }
     }
 
     pub fn try_new<M, K, V>(
         measurement: M,
-        field: K,
+        field: KeyName,
         value: V,
     ) -> Result<Self, NameRestrictionError>
     where
         M: TryInto<MeasurementName, Error = NameRestrictionError>,
-        K: Into<String>,
         V: Into<InfluxValue>,
     {
-        let fields = [(field.into(), value.into())].into_iter().collect();
+        let fields = [(field, value.into())].into_iter().collect();
         Ok(Self {
             measurement: measurement.try_into()?,
             tags: HashMap::new(),
             fields,
             timestamp: None,
-            _phantom: PhantomData,
         })
+    }
+
+    pub fn measurement(&self) -> &MeasurementName {
+        &self.measurement
+    }
+
+    pub fn tag<S>(&self, name: S) -> Option<&KeyName>
+    where
+        S: AsRef<str>,
+    {
+        self.tags
+            .iter()
+            .find_map(|(key, value)| (key.as_str() == name.as_ref()).then_some(value))
+    }
+
+    pub fn field<S>(&self, name: S) -> Option<&InfluxValue>
+    where
+        S: AsRef<str>,
+    {
+        self.fields
+            .iter()
+            .find_map(|(key, value)| (key.as_str() == name.as_ref()).then_some(value))
+    }
+
+    pub fn timestamp(&self) -> Option<DateTime<Utc>> {
+        self.timestamp
     }
 
     /// # Examples
@@ -64,10 +84,12 @@ impl InfluxLine {
     /// use influx_line::*;
     ///
     /// let some_time = Utc::now();
-    /// let line = InfluxLine::new("human".try_into().unwrap(), "age", 15)
+    /// let measurement = MeasurementName::new("human").unwrap();
+    /// let field = KeyName::new("age").unwrap();
+    /// let line = InfluxLine::new(measurement, field, 15)
     ///     .with_timestamp(some_time);
     ///
-    /// assert_eq!(some_time, line.timestamp.unwrap());
+    /// assert_eq!(some_time, line.timestamp().unwrap());
     /// ```
     pub fn with_timestamp<T>(mut self, timestamp: T) -> Self
     where
@@ -84,10 +106,11 @@ impl InfluxLine {
     /// ```rust
     /// use influx_line::*;
     ///
-    /// let line = InfluxLine::new("human".try_into().unwrap(), "age", 15);
+    /// let measurement = MeasurementName::new("human").unwrap();
+    /// let field = KeyName::new("age").unwrap();
+    /// let line = InfluxLine::new(measurement, field, 15);
     ///
-    /// assert!(line.tags.is_empty());
-    /// assert_eq!(line.tags.get("there are no tags yet, buddy"), None);
+    /// assert_eq!(line.tag("there are no tags yet, buddy"), None);
     /// ```
     ///
     /// ## Adding and overriding tags
@@ -95,20 +118,18 @@ impl InfluxLine {
     /// ```rust
     /// use influx_line::*;
     ///
-    /// let line = InfluxLine::new("human".try_into().unwrap(), "age", 15)
-    ///     .with_tag("club", "art")
-    ///     .with_tag("location", "siberia")
-    ///     .with_tag("club", "sports");
+    /// let measurement = MeasurementName::new("human").unwrap();
+    /// let field = KeyName::new("age").unwrap();
+    /// let line = InfluxLine::new(measurement, field, 15)
+    ///     .with_tag(KeyName::new("club").unwrap(), KeyName::new("art").unwrap())
+    ///     .with_tag(KeyName::new("location").unwrap(), KeyName::new("siberia").unwrap())
+    ///     .with_tag(KeyName::new("club").unwrap(), KeyName::new("sports").unwrap());
     ///
-    /// assert_eq!(line.tags.get("location").unwrap(), "siberia");
-    /// assert_eq!(line.tags.get("club").unwrap(), "sports");
-    /// assert_eq!(line.tags.get("not added yet lol"), None);
+    /// assert_eq!(line.tag("location").unwrap().as_ref(), "siberia");
+    /// assert_eq!(line.tag("club").unwrap().as_ref(), "sports");
+    /// assert_eq!(line.tag("not added yet lol"), None);
     /// ```
-    pub fn with_tag<S1, S2>(mut self, tag: S1, value: S2) -> Self
-    where
-        S1: Into<String>,
-        S2: Into<String>,
-    {
+    pub fn with_tag(mut self, tag: KeyName, value: KeyName) -> Self {
         self.tags.insert(tag.into(), value.into());
         self
     }
@@ -120,11 +141,12 @@ impl InfluxLine {
     /// ```rust
     /// use influx_line::*;
     ///
-    /// let line = InfluxLine::new("human".try_into().unwrap(), "age", 15);
+    /// let measurement = MeasurementName::new("human").unwrap();
+    /// let field = KeyName::new("age").unwrap();
+    /// let line = InfluxLine::new(measurement, field, 15);
     ///
-    /// assert_eq!(line.fields.len(), 1);
-    /// assert_eq!(line.fields.get("height"), None);
-    /// assert_eq!(line.fields.get("age").cloned().unwrap(), 15.into());
+    /// assert_eq!(line.field("height"), None);
+    /// assert_eq!(line.field("age").cloned().unwrap(), 15.into());
     /// ```
     ///
     /// ## Adding and overriding fields
@@ -132,24 +154,25 @@ impl InfluxLine {
     /// ```rust
     /// use influx_line::*;
     ///
-    /// let line = InfluxLine::new("human".try_into().unwrap(), "age", 15)
-    ///     .with_field("height", 1.82)
-    ///     .with_field("age", 55)
-    ///     .with_field("is_epic", true)
-    ///     .with_field("name", "armstrong");
+    /// let measurement = MeasurementName::new("human").unwrap();
+    /// let field = KeyName::new("age").unwrap();
+    /// let line = InfluxLine::new(measurement, field, 15)
+    ///     .with_field(KeyName::new("height").unwrap(), 1.82)
+    ///     .with_field(KeyName::new("age").unwrap(), 55)
+    ///     .with_field(KeyName::new("is_epic").unwrap(), true)
+    ///     .with_field(KeyName::new("name").unwrap(), "armstrong");
     ///
-    /// assert_eq!(line.fields.get("height").cloned().unwrap(), 1.82.into() );
-    /// assert_eq!(line.fields.get("age").cloned().unwrap(), 55.into());
-    /// assert_eq!(line.fields.get("is_epic").cloned().unwrap(), true.into() );
-    /// assert_eq!(line.fields.get("name").cloned().unwrap(), "armstrong".into());
-    /// assert_eq!(line.fields.get("non-existent"), None);
+    /// assert_eq!(line.field("height").cloned().unwrap(), 1.82.into() );
+    /// assert_eq!(line.field("age").cloned().unwrap(), 55.into());
+    /// assert_eq!(line.field("is_epic").cloned().unwrap(), true.into());
+    /// assert_eq!(line.field("name").cloned().unwrap(), "armstrong".into());
+    /// assert_eq!(line.field("non-existent"), None);
     /// ```
-    pub fn with_field<S, V>(mut self, field: S, value: V) -> Self
+    pub fn with_field<V>(mut self, field: KeyName, value: V) -> Self
     where
-        S: Into<String>,
         V: Into<InfluxValue>,
     {
-        self.fields.insert(field.into(), value.into());
+        self.fields.insert(field, value.into());
         self
     }
 }
