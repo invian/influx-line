48ce05c5424011b55c132ffc909a2cad40b4adac
DOCtorActoAntohich <fairy666death@mail.ru>
2024-06-17 18:19:03 +0300

    implement value

--- src/types/value.rs
+++ src/types/value.rs
@@ -1,21 +1,96 @@
-use crate::{InfluxInteger, InfluxUInteger};
+use std::str::FromStr;
 
-#[derive(Debug, Clone, PartialEq, derive_more::From)]
+use crate::{Boolean, InfluxInteger, InfluxUInteger, QuotedString};
+
+#[derive(Debug, Clone, PartialEq, derive_more::From, derive_more::Display)]
 pub enum InfluxValue {
+    #[display(fmt = "{:?}", _0)]
     #[from(types(f32))]
     Float(f64),
     #[from(types(i8, i16, i32, i64))]
     Integer(InfluxInteger),
     #[from(types(u8, u16, u32, u64))]
     UInteger(InfluxUInteger),
+    #[from(types(bool))]
+    Boolean(Boolean),
     #[from]
-    Boolean(bool),
-    #[from]
-    String(String),
+    String(QuotedString),
 }
+
+#[derive(Debug, thiserror::Error)]
+#[error("Failed to parse string as valid Influx Value")]
+pub struct InfluxValueParseError;
 
 impl From<&str> for InfluxValue {
     fn from(value: &str) -> Self {
         Self::String(value.into())
+    }
+}
+
+impl FromStr for InfluxValue {
+    type Err = InfluxValueParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(float) = s.parse::<f64>() {
+            return Ok(float.into());
+        }
+
+        if let Ok(integer) = s.parse::<InfluxInteger>() {
+            return Ok(integer.into());
+        }
+
+        if let Ok(uinteger) = s.parse::<InfluxUInteger>() {
+            return Ok(uinteger.into());
+        }
+
+        if let Ok(boolean) = s.parse::<Boolean>() {
+            return Ok(boolean.into());
+        }
+
+        if let Ok(quoted_string) = s.parse::<QuotedString>() {
+            return Ok(quoted_string.into());
+        }
+
+        Err(InfluxValueParseError)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::InfluxValue;
+
+    #[rstest::rstest]
+    #[case::sane_float("12.33", InfluxValue::Float(12.33))]
+    #[case::float_without_dots("1", InfluxValue::Float(1.0))]
+    #[case::negative_with_scientific_stuff("-1.234456e+78", InfluxValue::Float(-1.234456e+78))]
+    #[case::positive_int("125i", InfluxValue::Integer(125.into()))]
+    #[case::negative_int("-25565i", InfluxValue::Integer((-25565).into()))]
+    #[case::uint("999999999u", InfluxValue::UInteger((999999999 as u32).into()))]
+    #[case::le_true("true", InfluxValue::Boolean(true.into()))]
+    #[case::le_false("FALSE", InfluxValue::Boolean(false.into()))]
+    #[case::string("\"Dunno what to say\"", InfluxValue::String("Dunno what to say".into()))]
+    fn successful_parsing(#[case] input: &str, #[case] expected_value: InfluxValue) {
+        let actual_value = InfluxValue::from_str(input).expect("Must parse here");
+
+        assert_eq!(expected_value, actual_value);
+    }
+
+    #[rstest::rstest]
+    #[case::sane_float(17.0, "17.0")]
+    #[case::float_strange(25 as f32, "25.0")]
+    #[case::int(15, "15i")]
+    #[case::uint(0 as u32, "0u")]
+    #[case::le_true(true, "true")]
+    #[case::le_false(false, "false")]
+    #[case::string("eat \"this\" hehe", "\"eat \\\"this\\\" hehe\"")]
+    fn display<T>(#[case] value: T, #[case] expected_string: &str)
+    where
+        InfluxValue: From<T>,
+    {
+        let actual_string = InfluxValue::from(value).to_string();
+
+        assert_eq!(expected_string, actual_string);
     }
 }
